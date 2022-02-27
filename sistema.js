03c4816c6c775edd43eb00acc269b46d23fa5450
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-02-27 18:14:38 -0300

    Sistema com o funcionamento do botão da pág HTML.

--- sistema.js
+++ sistema.js
@@ -4,5 +4,12 @@ function adicionaAluno() {
     const nomeInserido = document.getElementById("nome")
     const nascInserido = document.getElementById("nasc")
 
-    
+    listaAlunos.push(
+        {nome: "nomeInserido",
+        nasc: "nascInserido"}
+    )
+
+    console.log(listaAlunos)
+
+    return listaAlunos
 }
