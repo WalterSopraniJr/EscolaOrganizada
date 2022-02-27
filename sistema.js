68065a9cc4b18e42ec56a32eea1727634457edf9
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-02-27 16:36:10 -0300

    Criado arquivo que executará execução das funções de funcionamento do sistema.

---
+++ sistema.js
@@ -0,0 +1,8 @@
+const listaAlunos = require('./listaAlunos')
+
+function adicionaAluno() {
+    const nomeInserido = document.getElementById("nome")
+    const nascInserido = document.getElementById("nasc")
+
+    
+}
