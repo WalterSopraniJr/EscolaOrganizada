7ab3f4a9d96bfb60772926444254a038a4b3dd1e
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-02-28 18:59:39 -0300

    Funções do botão adicionar revisadas.

--- sistema.js
+++ sistema.js
@@ -1,15 +1,17 @@
 const listaAlunos = require('./listaAlunos')
 
-function adicionaAluno() {
-    const nomeInserido = document.getElementById("nome")
-    const nascInserido = document.getElementById("nasc")
-
-    listaAlunos.push(
-        {nome: "nomeInserido",
-        nasc: "nascInserido"}
-    )
-
-    console.log(listaAlunos)
-
-    return listaAlunos
-}
+function adicionaAluno(e) {
+    e.preventDefault();
+  
+    const nomeInserido = document.getElementById("nome").value;
+    const nascInserido = document.getElementById("nasc").value;
+  
+    if (nomeInserido === "" || nascInserido === "")
+      return console.log("dados faltando");
+    listaAlunos.push({ nome: nomeInserido, nasc: nascInserido });
+  
+    console.log(listaAlunos);
+    return listaAlunos;
+  }
+  
+  const button = document.getElementById("buttonSubmit");
