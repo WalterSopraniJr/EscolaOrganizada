d779220963e36980299d77a3a646023a4d3f1b19
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-01 21:53:15 -0300

    Alunos adicionados a lista.

--- listaAlunos.js
+++ listaAlunos.js
@@ -1,47 +1,137 @@
 const listaAlunos = [ 
 {
     nome: "Nicolas Luzia",
-    nasc: "10/02/2012",
+    idade: "10", // "10/02/2012",
     ano: "4",
     sala: ""
 }, {
     nome: "Andre Souza",
-    nasc: "03/11/2015",
+    idade: "7", //"03/11/2015",
     ano: "1",
     sala: ""
 }, {
     nome: "Linara Albuquerque",
-    nasc: "05/05/2011",
+    idade: "11", //"05/05/2011",
     ano: "5",
     sala: ""
 }, {
     nome: "Beatriz Oliveira",
-    nasc: "13/05/2012",
+    idade: "10", //"13/05/2012",
     ano: "4",
     sala: ""
 }, {
     nome: "Roger Santos",
-    nasc: "02/02/2014",
+    idade: "8", //"02/02/2014",
     ano: "2",
     sala: ""
 }, {
     nome: "Toninho Rodrigues",
-    nasc: "25/06/2013",
+    idade: "9", //"25/06/2013",
     ano: "",
     sala: ""
 }, {
     nome: "José Carlos",
-    nasc: "4/10/2013",
+    idade: "9",  //"4/10/2013",
     ano: "",
     sala: ""
 }, {
     nome: "Carlos Mendes",
-    nasc: "19/09/2015",
+    idade: "7", //"19/09/2015",
     ano: "",
     sala: ""
 }, {
     nome: "Alfredo Albino",
-    nasc: "11/11/2011",
+    idade: "11",  //"11/11/2011",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Kelly Kapor",
+    idade: "10",  
+    ano: "",
+    sala: ""
+}, {
+    nome: "Jimmy O'Flanagan",
+    idade: "8",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Javier Azaghal",
+    idade: "7",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Samantha Almeida",
+    idade: "8",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Janaina Fernandes",
+    idade: "9",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Andreia Adriana",
+    idade: "8",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Paulo Paulino",
+    idade: "11",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Ben Jur",
+    idade: "10",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Camila Damiana",
+    idade: "9",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Ricardo Romério",
+    idade: "8",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Tina Cristina",
+    idade: "7",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Mordred Ivaine",
+    idade: "11",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Ruff Ghanor",
+    idade: "7",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Marcela Silva",
+    idade: "9",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Romulo Fernandez",
+    idade: "10",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Jorge Palliterio",
+    idade: "11",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Amanda Albuquerque",
+    idade: "8",
+    ano: "",
+    sala: ""
+}, {
+    nome: "Leticia Fonseca",
+    idade: "11",
     ano: "",
     sala: ""
 }
