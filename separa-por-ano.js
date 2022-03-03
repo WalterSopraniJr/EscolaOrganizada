3fc83ce8c971751ab72e62ae935c8b1be069c45b
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-03 06:09:35 -0300

    Aplicação de testes de mesa e modificação do push, para que ele receba o endereço de onde está o aluno na lista.

--- separa-por-ano.js
+++ separa-por-ano.js
@@ -10,21 +10,25 @@ const quintoAno = []
 function separaPorAno(array, idade, anoLetivo) {
 
     for (let index = 0; index <= array.length; index++) {
-        console.log(`aluno avaliado ${array[index].nome}, idade: ${array[index].idade}`)
+        //console.log(`aluno avaliado ${array[index].nome}, idade: ${array[index].idade}`)
         //console.log(array[index])
         let alunoAtual = array[index]
-        
+
         if (array[index].idade == idade) {
             console.log(anoLetivo[index])
-            anoLetivo.push(alunoAtual[index]) 
-                        
+            anoLetivo.push(index) 
+            return anoLetivo         
+               
         } else {
             index++
         }
       
     }
-    return anoLetivo, listaAlunos
+
+    console.log(`Os alunos do ano são: ${anoLetivo}`)
+
+    return array
 }
 
 console.log(separaPorAno(listaAlunos, 7, primeiroAno))
-console.log(`Os alunos do primeiro ano são: ${primeiroAno}`)
+//console.log(`Os alunos do primeiro ano são: ${primeiroAno}`)
